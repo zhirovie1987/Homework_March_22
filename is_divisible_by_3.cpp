7ef97c52dbdd5e57c4382671ef6e7b26a08ba013
zhirovie1987 <48652863+zhirovie1987@users.noreply.github.com>
2019-03-25 00:11:23 +0200

    task_5 added

---
+++ is_divisible_by_3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool Is_Divisible_By_3 (uint16_t data);
+
+uint32_t Get_Digits_Summ (uint32_t data);
+
+int main () {
+	
+	uint16_t Array[255];
+	uint32_t Summ(0);
+	bool Is_Divisible = false;
+	uint16_t Number_Quantity(0);
+
+	cin >> Number_Quantity;
+	
+	for (uint16_t i = 0; i < Number_Quantity; ++i) {
+		cin >> Array[i];
+		Summ += Array[i];
+	}
+	
+	if (Is_Divisible_By_3 (Get_Digits_Summ (Summ))) {
+		Summ = 0;
+		for (auto i = 0; i < Number_Quantity; ++i) {
+		Summ += Array[i] * pow (10, i);
+		}
+		cout << "Yes "<< Summ;;
+	}
+	
+	else {
+		cout << "No";
+	}	
+	
+	return 0;
+}
+
+bool Is_Divisible_By_3 (uint16_t data) {
+	if (!(data % 3)) {
+		return true;
+	}
+	return false;
+}
+
+uint32_t Get_Digits_Summ (uint32_t data) {
+	
+	uint32_t Summ = 0;
+	
+	while (data > 0) {
+		Summ += data % 10;
+		data /= 10;
+	}
+	return Summ;
+}
