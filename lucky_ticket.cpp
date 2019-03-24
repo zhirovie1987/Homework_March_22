a7ced4a47e536114a0e353870a6ec3e277a8ce55
zhirovie1987 <48652863+zhirovie1987@users.noreply.github.com>
2019-03-24 20:48:34 +0200

    task_0 added

---
+++ lucky_ticket.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+const auto MAGIC_NUMBER(999999);
+const auto DIGITS_NUMBER(6);
+
+bool Is_Lucky_Number (uint32_t data);
+
+int main () {
+	
+	uint32_t Value(0);
+	
+	cin >> Value;
+	
+	if (!(Value > MAGIC_NUMBER)) {
+		if (Is_Lucky_Number (Value)) {
+			cout << "Is lucky! Grab some beer to selebrate!";
+		}
+		else {
+			cout << "Isn't lucky. Oh.. What a shitty day.. So, grab some beer to console yourself :-)";
+		}
+	}
+	return 0;
+}
+
+bool Is_Lucky_Number (uint32_t data) {
+	
+	uint16_t L_Nibble_Summ(0), H_Nibble_Summ(0);
+	
+	for (auto i = DIGITS_NUMBER; i > 0; --i) {
+		if (i > DIGITS_NUMBER / 2) {
+			H_Nibble_Summ += data % 10;
+		}
+		else {
+			L_Nibble_Summ += data % 10;
+		}
+		data /= 10;
+	}
+	
+	if (H_Nibble_Summ == L_Nibble_Summ) {
+		return true;
+	}
+	return false;
+}
