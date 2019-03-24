78502db672eb445225b9c2526d7fcd8d7d8862f9
zhirovie1987 <48652863+zhirovie1987@users.noreply.github.com>
2019-03-24 21:42:47 +0200

    task_3 added

---
+++ is_bit_set.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	uint32_t Value(0);
+	auto Bit_Number(32);
+	
+	cout << "Put value: ";
+	cin >> Value;
+	cout << "Put bit number: ";
+	cin >> Bit_Number;
+	
+	if (Value & (0x1 << Bit_Number - 1)) {
+		cout << "Yes";
+	}
+	else {
+		cout << "No";
+	}
+	
+	return 0;
+}
