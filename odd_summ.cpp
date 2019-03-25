6cd738f63bda47859eba2fbe83dc7231e39d7302
zhirovie1987 <48652863+zhirovie1987@users.noreply.github.com>
2019-03-25 19:49:10 +0200

    исправления 25.03.19
    - исправлена инициализация переменных;
    - объявление Input_Buff после проверки кол-ва вводимых значений;
    - замена for на while в цикле считывания значений.

--- odd_summ.cpp
+++ odd_summ.cpp
@@ -4,14 +4,29 @@ using namespace std;
 
 int main () {
 	
-	int32_t Odd_Numbers_Summ(0);
-	auto Numbers_Quantity(50);
-	auto Input_Buff(-90);
+	int32_t Odd_Numbers_Summ = 0;
+	auto Numbers_Quantity = 50;
+	
 	
 	cout << "Enter quantity of numbers: ";
 	cin >> Numbers_Quantity;
 	
 	if (Numbers_Quantity >= 1 && Numbers_Quantity <= 50) {
+		auto Input_Buff = -60;
+		while (Numbers_Quantity) {
+			cin >> Input_Buff;
+			if ((Input_Buff >= -60) && (Input_Buff <= 90)) {
+				if (Input_Buff & 0x1) {
+					Odd_Numbers_Summ += Input_Buff;
+				}
+				--Numbers_Quantity;
+			}
+			else {
+				++Numbers_Quantity;
+				cout << "Out of range, try again" << endl;	
+			}
+		}
+		/*
 		for (auto i = Numbers_Quantity; i > 0 ; --i) {
 			cin >> Input_Buff;
 			if ((Input_Buff >= -60) && (Input_Buff <= 90)) {
@@ -24,6 +39,7 @@ int main () {
 				cout << "Out of range, try again" << endl;	
 			}
 		}
+		*/
 		cout << "Odd numbers summ = " << Odd_Numbers_Summ;
 	}
 	
