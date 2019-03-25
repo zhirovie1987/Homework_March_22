292e2a14430e42f326b55ca1f6b573672cb9e34c
zhirovie1987 <48652863+zhirovie1987@users.noreply.github.com>
2019-03-25 19:18:35 +0200

    исправления 25.03.19
    - исправлена инициализация переменных;
    - убрано форматирование вывода.

--- reverse.cpp
+++ reverse.cpp
@@ -5,18 +5,18 @@ using namespace std;
 int32_t Reverse (int32_t data, uint16_t &digits);
 
 int main () {
-	int32_t Number_To_Reverse(0);
-	uint16_t Digits_Of_Number(0);
-	int32_t Result(0);
+	int32_t Number_To_Reverse = 0;
+	uint16_t Digits_Of_Number = 0;
+	int32_t Result = 0;
 	cin >> Number_To_Reverse;
 	Result = Reverse (Number_To_Reverse, Digits_Of_Number);
-
-	cout << setfill('0') << setw(Digits_Of_Number) << Result;
+	cout << Result;
+	//cout << setfill('0') << setw(Digits_Of_Number) << Result;
 	return 0;
 }
 
 int32_t Reverse (int32_t data, uint16_t &digits) {
-	int32_t temp(0);
+	int32_t temp = 0;
 	digits = 0;
 	while (data) {
 		temp = (temp * 10) + (data % 10);
@@ -24,5 +24,4 @@ int32_t Reverse (int32_t data, uint16_t &digits) {
 		++digits;
 	}
 	return temp;
-	
 }
