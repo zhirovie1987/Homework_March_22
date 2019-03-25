38af17c07124960a4ba545d220c1ab47b216780c
zhirovie1987 <48652863+zhirovie1987@users.noreply.github.com>
2019-03-25 20:13:44 +0200

    исправления 25.03.19
    - учтены замечания по Current_Summ

--- the best_divider.cpp
+++ the best_divider.cpp
@@ -6,21 +6,22 @@ uint32_t Get_Digits_Summ (uint32_t data);
 
 int main () {
 	
-  uint32_t Value(0);
-  uint32_t Divider(1);
-  uint32_t Current_Summ(0), Previous_Summ(0);
-  uint32_t Best_Divider(0);
+  uint32_t Value = 0;
+  uint32_t Divider = 1;
+  uint32_t Previous_Summ = 0;
+  uint32_t Best_Divider = 0;
   
   cin >> Value;
   
   while (Divider <= Value) {
+  	uint32_t Current_Summ = 0;
   	if (!(Value % Divider)) {
   		Current_Summ = Get_Digits_Summ (Divider);
   		cout << Divider << endl;
   		if (Current_Summ >= Previous_Summ) {
   			Best_Divider = Divider;
+  			Previous_Summ = Current_Summ;
 		}
-		Previous_Summ = Current_Summ;
 	  }
   	++Divider;
   }
