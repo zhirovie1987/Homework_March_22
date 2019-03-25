1173b9fe21abed6526c85f7f1fad7729a7f293fc
zhirovie1987 <48652863+zhirovie1987@users.noreply.github.com>
2019-03-25 20:03:37 +0200

    исправления 25.03.19
    - исправлена инициализация переменных;
    - добавлена проверка корректности номера бита.

--- is_bit_set.cpp
+++ is_bit_set.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 int main () {
-	uint32_t Value(0);
-	auto Bit_Number(32);
+	uint32_t Value = 0;
+	auto Bit_Number = 32;
 	
 	cout << "Put value: ";
 	cin >> Value;
 	cout << "Put bit number: ";
 	cin >> Bit_Number;
 	
-	if (Value & (0x1 << Bit_Number - 1)) {
+	if ((Value > 0) && (Value <= 32) && (Value & (0x1 << Bit_Number - 1))) {
 		cout << "Yes";
 	}
 	else {
